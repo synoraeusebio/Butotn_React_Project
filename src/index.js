7c1ecdd98e9f9a0f3623ec5f5afe2b474b1b2b1a
synoraeusebio <Synora.Eusebio@gmail.com>
2022-07-21 11:06:01 -0400

    changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
+/*
+Create a button using React syntax: 
+When the button is clicked on, an alert winder should appear
+
+Notes:
+Button text: "Click me!"
+Alert window text: "Congrats! The button worked!"
+
+What needs to be done: 
+1. Create a folder to hold project files
+2. Install Create React App in project folder
+  Instructions: https://reactjs.org/docs/create-a-new-react-app.html#create-react-app
+3. Find out which events can handle the click of a button
+  -onClick
+  -handleClick
+4. Find out how to make the alert window to appear when the button is clicked on
+
+Don't forget: 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+(I initally forgot do import these :)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+Optional: 
+CSS for the button?
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+//<div id="root"></div>
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root')
+//   );
+
+//   const element = <h1>Hello, world!</h1>;
+//   root.render(element)
+  
+
+//create button
+
+
+class MyButton extends React.Component {
+   
+  render() {
+      const shoot = () => {
+      alert("You did it!");
+    }
+    //return <h1>Button will go here??</h1>
+    return (
+      <button onClick={shoot}>Click me!</button>
+      );
+  }
+}
+
+//display the button on the screen
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyButton />);
+
+//When button is clicked, show an alert window with text
+
+
